30f441e4d020e9fba05b1a34eff7bfe2afb0bb58
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-20 22:02:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+const p = document.getElementById("timer");
+const newDate = new Date();
+
+const options = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: true, // Use 24-hour format
+};
+
+const formattedDate = newDate.toLocaleString(undefined, options);
+
+p.innerText = formattedDate;
